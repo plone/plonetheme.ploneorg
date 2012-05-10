a9a120d98eec3e42acb9f4f1e163f8a81adf1016
Alex Clark <aclark@aclark.net>
2012-05-10 10:35:43 -0400

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import find_packages
 from setuptools import setup
+import os
 
-VERSION='0.0.1'
+VERSION='0.0.2'
 
 
 setup(
+    description='Plone.org theme',
     entry_points={
         'z3c.autoinclude.plugin': 'target = plone', 
     },
+    include_package_data=True,
     install_requires=[
         'setuptools',
     ],
+    long_description=(
+        open('README.rst').read() +
+        open(os.path.join('docs', 'HISTORY.txt')).read()
+    ),
     name='plonetheme.ploneorg',
     namespace_packages=[
         'plonetheme',
