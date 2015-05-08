fca364e617874933c2612b8144862b731d1af806
Alex Clark <aclark@aclark.net>
2015-05-08 17:11:54 -0400

    Bump
    Deploy bug fix from vangheem; make pyroma happy

---
+++ plonetheme/ploneorg/tests.py
@@ -0,0 +1,5 @@
+from unittest import TestSuite
+
+
+class test_suite(TestSuite):
+    pass
--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-VERSION='0.0.4'
+VERSION='0.0.5'
 
 
 setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
     description='Plone.org theme',
+    classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+    ],
     entry_points={
         'z3c.autoinclude.plugin': 'target = plone', 
     },
@@ -14,6 +22,8 @@ setup(
     install_requires=[
         'setuptools',
     ],
+    keywords='plone theme zope',
+    license='GPL2',
     long_description=(
         open('README.rst').read() +
         open(os.path.join('docs', 'HISTORY.txt')).read()
@@ -23,5 +33,8 @@ setup(
         'plonetheme',
     ],
     packages=find_packages(),
+    test_suite='plonetheme.ploneorg.tests.test_suite',
+    url='https://github.com/plone/plonetheme.ploneorg',
     version=VERSION,
+    zip_safe=False,
 )
